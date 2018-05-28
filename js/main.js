06267551d4e2be0ed08c6ef23555c4dc0b32635a
Ramon Oliver <ramon.oliver@gmail.com>
2018-05-28 16:17:29 -0300

    Página de perfil

--- js/main.js
+++ js/main.js
@@ -94,4 +94,23 @@
         $(this).toggleClass('special');
     });
 
+    //Carousel
+
+    $('.carousel[data-type="multi"] .item').each(function() {
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+
+        for (var i = 0; i < 2; i++) {
+            next = next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+
+            next.children(':first-child').clone().appendTo($(this));
+        }
+    });
+
 })(jQuery);
